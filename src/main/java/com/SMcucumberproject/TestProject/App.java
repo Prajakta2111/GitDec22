03e96e7acb23f3b6960ebf4e629d965db8c450bf
Prajakta2111 <patilprajakta2111@gmail.com>
2022-06-03 19:03:40 +0530

    First commit

---
+++ src/main/java/com/SMcucumberproject/TestProject/App.java
@@ -0,0 +1,13 @@
+package com.SMcucumberproject.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
